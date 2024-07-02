881a08cf1f97d4fba57c94858a7609c6bca0d3a8
himajareddy142 <himajareddy0110@gmail.com>
2024-07-02 15:39:34 +0530

    2 Strings

---
+++ Test3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.String;
+class Test3
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str1=s.nextLine();
+String str2=s.nextLine();
+if(str1.contains(str2))
+System.out.println("Yes");
+else
+System.out.println("No");
+}
+}
