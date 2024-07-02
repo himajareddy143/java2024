1182da429dae7b69e9cd57644667e60ff4253d99
himajareddy142 <himajareddy0110@gmail.com>
2024-07-02 15:33:11 +0530

    String comparision

---
+++ Test9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.String;
+class Test9
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str="java is simple language";
+System.out.println(str.contains("simple"));
+System.out.println(str.contains("python"));
+System.out.println(str.contains("lan"));
+System.out.println(str.startsWith("j"));
+System.out.println(str.startsWith("A"));
+System.out.println(str.endsWith("age"));
+}
+}
