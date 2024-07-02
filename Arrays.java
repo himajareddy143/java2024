6449b917c155955ad1c226dc162d18bb0dabd51c
himajareddy142 <himajareddy0110@gmail.com>
2024-07-02 14:57:43 +0530

    ARRAYS

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Arrays
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int n,count=0;
+n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+if(a[0]<=a[i])
+{
+count++;
+}
+}
+System.out.println(count);
+}
+}
+
