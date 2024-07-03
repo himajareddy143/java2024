b201d8747f3c00b5a0617feb9557b84ce2abfc4a
himajareddy142 <himajareddy0110@gmail.com>
2024-07-03 14:27:45 +0530

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.String;
+class Test7
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.readLine();
+String res[]=str.split(" ");
+for(int i=0;i<res.length;i++)
+{
+System.out.println(res[i]);
+}
+}
