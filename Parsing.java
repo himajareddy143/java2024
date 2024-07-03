15d8245861a49937337ac7346905f00dab30d523
himajareddy142 <himajareddy0110@gmail.com>
2024-07-03 13:58:13 +0530

    Parsing

---
+++ Parsing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.String;
+class Parsing
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str1="234";
+String str2="563";
+System.out.println(Integer.parseInt(str1)+Integer.parseInt(str2));
+}
+}
