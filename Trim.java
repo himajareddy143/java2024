342c1e6a472d516cdbb499e059711ef3d2edb48e
himajareddy142 <himajareddy0110@gmail.com>
2024-07-03 13:56:01 +0530

    Trim

---
+++ Trim.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.String;
+class Trim
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str="     java    lang   ";
+System.out.println(str.length());
+String str1=str.trim();
+System.out.println(str1.length());
+System.out.println(str1);
+}
+}
